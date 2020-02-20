b6c622064c59036a7ee0a45a562b274c81657a68
woodchuckchoi <choiggoggi@gmail.com>
2020-02-21 08:42:12 +0900

    credentials feature added, will find credentials path and region path using default values

--- logicalTerraform.py
+++ logicalTerraform.py
@@ -1,17 +1,19 @@
-class Terraform:
-    self.body = ""
-    self.tf_file = ""
-    self.elements = []
+import os
 
-    def __init__(self):
+class Terraform:
+    body = ""
+    tf_file = ""
+    elements = []
+
+    def __init__(self, credentials_path=os.path.join(os.environ["HOME"], ".aws", "credentials"), region="ap-northeast-2"):
         self.body = '''
-        provider "aws" {
+        provider "aws" {{
             profile     = "aws_provider"
-            region      = var.my_region
-            access_key  = var.my_access_key
-            secret_key  = var.my_secret_key
-        }
-        '''
+            region      = {}
+            credentials = {}
+        }}
+        
+        '''.format(region, credentials_path)
 
     def publish(self):
         if len(self.body) == 0: 
@@ -28,7 +30,7 @@ class Terraform:
             self.body += "\n" + entry["text"]
 
     def add(self, element):
-        self.element.append(element)
+        self.elements.append(element)
 
     def gen_vpc(self, name, cidr):
         return {
@@ -36,10 +38,10 @@ class Terraform:
             "name"  : name,
             "cidr"  : cidr,
             "text"  : '''
-            resource "aws_vpc" "{}" {
+            resource "aws_vpc" "{}" {{
                 cidr_block              = "{}"
                 enable_dns_hostnames    = "true"
-            }
+            }}
             '''.format(name, cidr)
         }
     
@@ -49,9 +51,9 @@ class Terraform:
             "name"      : name,
             "vpc_name"  : vpc_name,
             "text"      : '''
-            resource "aws_internet_gateway" "{}" {
+            resource "aws_internet_gateway" "{}" {{
                 vpc_id = aws_vpc.{}.id
-            }
+            }}
             '''.format(name, vpc_name)
         }
     
@@ -66,16 +68,16 @@ class Terraform:
             "az"        : az,
             "public"    : public,
             "text"      : '''
-            resource "aws_subnet" "{}" {
+            resource "aws_subnet" "{}" {{
                 vpc_id                  = aws_vpc.{}.id
                 cidr_block              = "{}"
                 availability_zone       = "{}"
                 map_public_ip_on_launch = {}
-            }
+            }}
             '''.format(name, vpc_name, cidr, az, public_ip)
         }
 
-    def gen_eip(self, name, vpc=True, depend_igw_name):
+    def gen_eip(self, name, depend_igw_name, vpc=True):
         in_vpc = "false"
         if vpc: in_vpc = "true"
         return {
@@ -84,10 +86,10 @@ class Terraform:
             "vpc"   : vpc,
             "depend": depend_igw_name,
             "text"  : '''
-            resource "aws_eip" "{}" {
+            resource "aws_eip" "{}" {{
                 vpc         = {}
                 depends_on  = [aws_internet_gateway.{}]
-            }
+            }}
             '''.format(name, in_vpc, depend_igw_name)
         }
 
@@ -98,10 +100,10 @@ class Terraform:
             "eip_name"      : eip_name,
             "subnet_name"   : subnet_name,
             "text"          : '''
-            resource "aws_nat_gateway" "{}" {
+            resource "aws_nat_gateway" "{}" {{
                 allocation_id   = aws_eip.{}.id
                 subnet_id       = aws_subnet.{}.id
-            }
+            }}
             '''.format(name, eip_name, subnet_name)
         }
 
@@ -109,23 +111,23 @@ class Terraform:
         if (not (igw_name or natgw_name)) or (igw_name and natgw_name): raise ValueError
         
         text = '''
-        resource "aws_route_table" "{}" {
+        resource "aws_route_table" "{}" {{
             vpc_id = aws_vpc.{}.id
-            route {
+            route {{
                 cidr_block = "{}"
                 nat_gateway_id = aws_nat_gateway.{}.id
-            }
-        }
+            }}
+        }}
         '''.format(name, vpc_name, route_cidr, natgw_name)
        
         if igw_name: text = '''
-            resource "aws_route_table" "{}" {
+            resource "aws_route_table" "{}" {{
                 vpc_id = aws_vpc.{}.id
-                route {
+                route {{
                     cidr_block = "{}"
                     gateway_id = aws_internet_gateway.{}.id
-                }
-            }
+                }}
+            }}
         '''.format(name, vpc_name, route_cidr, igw_name)
 
         return {
@@ -145,16 +147,16 @@ class Terraform:
             "subnet_name"       : subnet_name,
             "route_table_name"  : route_table_name,
             "text"              : '''
-            resource "aws_route_table_association" "{}" {
+            resource "aws_route_table_association" "{}" {{
                 subnet_id       = aws_subnet.{}.id
                 route_table_id  = aws_route_table.{}.id
-            }
+            }}
             '''.format(name, subnet_name, route_table_name)
         }
     
     def gen_security_group(self, name, vpc_name, route):
         text = '''
-        resource "aws_security_group" "{}" {
+        resource "aws_security_group" "{}" {{
             name    = "{}"
             vpc_id  = aws_vpc.{}.id
      
@@ -170,16 +172,16 @@ class Terraform:
             blocks = blocks[:-1] + "]"
 
             text += '''
-                {} {
+                {} {{
                     from_port   = {}
                     to_port     = {}
                     protocol    = "{}"
                     cidr_blocks = {}
-                }
+                }}
             '''.format(entry["direction"], entry["from"], entry["to"], entry["protocol"], blocks)
         
         text += '''
-        }
+        }}
         '''
         
         return {
@@ -203,14 +205,14 @@ class Terraform:
             "subnet_name"           : subnet_name,
             "public"                : public,
             "text"                  : '''
-            resource "aws_instance" "{}" {
+            resource "aws_instance" "{}" {{
                 instance_type               = "{}"
                 ami                         = "{}"
                 key_name                    = var.key_name
                 vpc_security_group_ids      = [aws_security_group.{}.id]
                 subnet_id                   = aws_subnet.{}.id
                 associate_public_ip_address = {}
-            }
+            }}
             '''.format(name, instance_type, ami, security_group_name, subnet_name, public_ip)
         }
 
@@ -223,21 +225,21 @@ class Terraform:
             "vpc_name"      : vpc_name,
             "health_port"   : health_port,
             "text"          : '''
-            resource "aws_alb_target_group" "{}" {
+            resource "aws_alb_target_group" "{}" {{
                 name    = "{}"
                 port    = {}
                 protocol= "{}"
                 vpc_id  = aws_vpc.{}.id
 
-                health_check {
+                health_check {{
                     healthy_threshold   = 5
                     unhealthy_threashold= 3
                     timeout             = 5
                     path                = var.target_group_path
                     interval            = 30
                     port                = {}
-                }
-            }
+                }}
+            }}
             '''.format(name, name, port, protocol, vpc_name, health_port)
         }
     
@@ -249,15 +251,15 @@ class Terraform:
             "target_name"       : target_name,
             "port"              : port,
             "text"              : '''
-            resource "aws_alb_target_group_attachment" "{}" {
+            resource "aws_alb_target_group_attachment" "{}" {{
                 target_group_arn    = aws_alb_target_group.{}.arn
                 target_id           = aws_instance.{}.id
                 port                = {}
-            }
+            }}
             '''.format(name, target_group_name, target_name, port)
         }
 
-    def gen_load_balancer(self, name, load_balancer_type, internal=True, subnets_names, security_group_name):
+    def gen_load_balancer(self, name, load_balancer_type, subnets_names, security_group_name, internal=True):
         internal_text = "false"
         if internal: internal_text = "true"
 
@@ -268,13 +270,13 @@ class Terraform:
         subnets = subnets[:-1] + "]"
 
         text = '''
-        resource "aws_lb" "{}" {
+        resource "aws_lb" "{}" {{
             name                = "{}"
             internal            = {}
             load_balancer_type  = "{}"
             subnets             = {}
             security_groups     = [aws_security_group.{}.id]
-        }
+        }}
         '''.format(name, name, internal_text, load_balancer_type, subnets, security_group_name)
         
         return {
@@ -293,7 +295,7 @@ class Terraform:
             "name"                  : name,
             "security_group_name"   : security_group_name,
             "text"                  : '''
-            resource "aws_db_instance" "{}" {
+            resource "aws_db_instance" "{}" {{
                 allocated_storage       = 20
                 engine                  = "mysql"
                 engine_version          = "5.7.26"
@@ -304,7 +306,7 @@ class Terraform:
                 vpc_security_group_ids  = [aws_security_group.{}.id]
                 skip_final_snapshot     = true
                 multi_az                = true
-            }
+            }}
             '''.format(name, security_group_name)
         }
 
@@ -315,15 +317,15 @@ class Terraform:
             "ami"   : ami,
             "az"    : az,
             "text"  : '''
-            resource "aws_launch_template" "{}" {
+            resource "aws_launch_template" "{}" {{
                 name            = "{}"
                 image_id        = "{}"
                 instance_type   = "t2.micro"
 
-                placement {
+                placement {{
                     availability_zone   = "{}"
-                }
-            }
+                }}
+            }}
             '''.format(name, name, ami, az)
         }
 
@@ -342,7 +344,7 @@ class Terraform:
             "azs"                   : azs,
             "launch_template_name"  : launch_template_name,
             "text"                  : '''
-            resource "aws_autoscaling_group" "{}" {
+            resource "aws_autoscaling_group" "{}" {{
                 name                        = "{}"
                 desired_capacity            = 4
                 max_size                    = 2
@@ -351,10 +353,10 @@ class Terraform:
                 health_check_type           = "ELB"
                 availability_zones          = {}
 
-                launch_template {
+                launch_template {{
                     id      = aws_launch_template.{}.id
                     version = "latest"
-                }
-            }
+                }}
+            }}
             '''.format(name, name, az_list, launch_template_name)
         }
